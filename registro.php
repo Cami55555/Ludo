b65ce6363ac2ffadb4fb150b6bd9b3955b7d048a
Cami55555 <est.lozano.camila@latecnicalf.com.ar>
2025-10-13 16:14:02 -0300

    registro.php

--- registro.php
+++ registro.php
@@ -7,13 +7,13 @@ if (file_exists($usuarios)) {    //revisa que exista ese archivo
 $aux = 0;     //para contar cuántos datos coinciden con los ya ingresados de modo en que si todos coinciden no se agrega un nuevo usuario
 $auxMail = false;
 $auxU = false;
-foreach ($users as $k => $u) {
+foreach ($users as $k => $u) {  // revisa todos los datos del json y los compara con los recién ingresados y si coinciden suma números a $aux y si coinciden ya sea mail o nombre de usuario entonces directamente hace que no le permita registrarse con esos datos
     if ($u[$k] == $_POST[$k]) {
         if ($k === 'mail') {
             $aux = 6;
             $auxMail = false;
         } else if ($k === 'usuario') {
-            $aux++;
+            $aux = 6;
             $auxU = false;
         } else {
             $aux++;
@@ -22,7 +22,7 @@ foreach ($users as $k => $u) {
         }
     }
 }
-if ($aux < 6 && $auxMail === true && $auxU === true) {
+if ($aux < 6 && $auxMail === true && $auxU === true) {  // si no hay datos repetidos, guarda la nueva cuenta en el json de usuarios
     $nuevoDato = [
         "nombre" => $_POST['nombre'],
         "apellido" => $_POST['apellido'],
@@ -34,9 +34,9 @@ if ($aux < 6 && $auxMail === true && $auxU === true) {
     ];
     $users[] = $nuevoDato;
     file_put_contents($usuarios, json_encode($users, JSON_PRETTY_PRINT));       //se guardan los datos del nuevo usuario en el archivo usuarios.json
-} else if ($aux == 6) {
-    die("El usuario ya está registrado, debe <a href='login.php'>logearse</a>");
-} else if ($auxMail === false) {
+} else if ($aux >= 6) {
+    die("El usuario ya está registrado, debe <a href='login.php'>logearse</a>");            // si todos los datos coinciden entonces le dice que se logee porque su cuenta ya está registrada
+} else if ($auxMail === false) {                          // con el mero hecho de que el mail o el nombre de usuario ya estén en uso debe volver a registrarse pero cambiando esos datos
     die("El mail ya está en uso. <a href='registrarse.php'>Registro</a>");
 } else if ($auxU === false) {
     die("El nombre de usuario ya está en uso. <a href='registrarse.php'>Registro</a>");
