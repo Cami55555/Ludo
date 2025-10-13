68d168b98fa92c67770756a822968660b21bc462
Cami55555 <est.lozano.camila@latecnicalf.com.ar>
2025-10-13 17:39:54 -0300

    login.php

--- login.php
+++ login.php
@@ -10,10 +10,13 @@ $usuario = $_POST['usuario'];
 $mail = $_POST['mail'];
 $clave = $_POST['clave'];
 $rep_clave = $_POST['clave2'];
-// se comparan los datos ingresados con los del json y de coincidir lleva al usuario de nuevo al index
+// se comparan los datos ingresados con los del json y de coincidir los guarda en variables de sesión lleva al usuario de nuevo al index
 foreach ($users as $u)
 {
     if ($users['usuario'] === $usuario && $users['mail'] === $mail && $users['clave'] === $clave && $clave === $rep_clave){
+        $_SESSION['usuario'] = $usuario;
+        $_SESSION['mail'] = $mail;
+        $_SESSION['wins'] = $users['wins'];
         header('Location:index.php');
     } else {
         die("El usuario no se encontró. Verifique que los datos ingresados sean correctos en el <a href='login.html'>login</a> o si no está registrado <a href='registrarse.php'>registrese</a>");
